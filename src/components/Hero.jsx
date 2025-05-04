ff70fd0dea975d34aa0f22b450081855c2430a25
Ashok <ashok99@gmail>
2025-05-04 23:39:23 +0530

    second changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Hero.css";
+import myImage from "../assets/mypic.jpeg";
 
 function Hero() {
   return (
     <section className="hero">
       <div className="hero-left">
-        <h1>Hello, I'm <span>Ashok Vempalli</span></h1>
+        <h1>
+          Hello, I'm <span>Ashok Vempalli</span>
+        </h1>
         <h2>FrontEnd Developer 🚀</h2>
 
         <p className="hero-description">
@@ -13,21 +16,29 @@ function Hero() {
         </p>
 
         <div className="social-icons">
-          <a href="https://www.youtube.com/@AshokvAshu"><i className="fab fa-youtube"></i></a>
-          <a href="#"><i className="fab fa-linkedin"></i></a>
-          <a href="#"><i className="fab fa-instagram"></i></a>
-          <a href="#"><i className="fab fa-github"></i></a>
+          <a href="https://www.youtube.com/@AshokvAshu">
+            <i className="fab fa-youtube"></i>
+          </a>
+          <a href="#">
+            <i className="fab fa-linkedin"></i>
+          </a>
+          <a href="#">
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href="#">
+            <i className="fab fa-github"></i>
+          </a>
         </div>
 
-        <button className="hero-button"><a href="#resume" className="a">Resume</a></button>
+        <button className="hero-button">
+          <a href="#resume" className="a">
+            Resume
+          </a>
+        </button>
       </div>
 
       <div className="hero-right">
-        <img 
-          src="/images/mypic.jpeg" // <-- Direct path ninchi manual image vey
-          alt="Hero Ashu" 
-          className="hero-image" 
-        />
+        <img src={myImage} alt="Hero Ashu" className="hero-image" />
       </div>
     </section>
   );
